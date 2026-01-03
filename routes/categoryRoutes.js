484675ecf7ee0a4ead8dc559db34ab96a9c07c04
LailaNounouh <laila.nounouh@student.ehb.be>
2026-01-03 01:00:34 +0100

    feat: apply category validation middleware to routes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const ctrl = require('../controllers/categoryController');
+const { validateCategoryCreate, validateCategoryUpdate } = require('../middleware/validation');
 const router = express.Router();
 
 router.get('/', ctrl.getAll);
 router.get('/:id', ctrl.getOne);
-router.post('/', ctrl.create);
-router.put('/:id', ctrl.update);
+router.post('/', validateCategoryCreate, ctrl.create);
+router.put('/:id', validateCategoryUpdate, ctrl.update);
 router.delete('/:id', ctrl.remove);
 
 module.exports = router;
